b3692af85a01d84f13758d7df793f2f9b240e855
Rajeev Singh <rajkumar888@gmail.com>
2022-11-20 22:33:20 +0800

    Second time commit for demo

--- src/test1/HelloWorldDemo.java
+++ src/test1/HelloWorldDemo.java
@@ -4,8 +4,15 @@ public class HelloWorldDemo {
 
 	public static void main(String[] args) {
 
-		
-		System.out.println("welcome to java learning......");
+		System.out.println("welcome to java learning......1");
+
+		System.out.println("welcome to java learning......2");
+
+		System.out.println("welcome to java learning......3");
+
+		System.out.println("welcome to java learning......4");
+
+		System.out.println("welcome to java learning......5");
 	}
 
 }
